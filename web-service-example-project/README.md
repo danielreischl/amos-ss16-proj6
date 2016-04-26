cbec73daba723bef19ff1027e2661494d5f0f92b
tobdor <tobias.dorsch@googlemail.com>
2016-04-26 12:01:33 +0200

    Added web service example

---
+++ web-service-example-project/README.md
@@ -0,0 +1,2 @@
+# web-service-example-project
+Example of a web service which can be deployed on the OSR group's servers.
---
+++ web-service-example-project/src/main/java/de/fau/cs/osr/web_example/ctrl/HomeController.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2016 The Open Source Research Group,
+ *                University of Erlangen-Nürnberg
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package de.fau.cs.osr.web_example.ctrl;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+	@RequestMapping("/")
+	public String loadHomePage(Model m) {
+		return "home";
+	}
+}
