d960bb13752561d9409fec1f1f2ed135a26af654
Peter Tan <dead-master@msn.com>
2016-05-02 18:34:55 +0200

    1. Created folder structure for our Client-side code 2. moved  the graph example from tobi to Client/sections 3. created initial index.html , css and angularjs files
    Not finished yet:
    - Routing of the html files
    - inclusion of tobi's demo graph into the main page
    - Layout of the Application

---
+++ Client/main.ctrl.js
@@ -0,0 +1,6 @@
+/* This file should list all main controllers */
+
+angular.module('app').controller("MainController", function(){
+    var vm = this;
+    vm.title = 'Welcome to the Main Page';
+});
