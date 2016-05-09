665cab347bebaf35482cb7c8ed43317fec96ca98
Peter Tan <dead-master@msn.com>
2016-05-09 21:09:17 +0200

    Improved static Layout of the WebApp. changed Side navigation and used a new layout concept

--- Client/main.ctrl.js
+++ Client/main.ctrl.js
@@ -1,6 +1,16 @@
 /* This file should list all main controllers */
 
-angular.module('app').controller("MainController", function(){
+angular.module('app')
+
+.controller("MainController", function(){
     var vm = this;
     vm.title = 'Welcome to the Main Page';
-});
+})
+
+.controller('sideNavController', sideNavController);
+     function sideNavController ($scope, $mdSidenav) {
+        $scope.openLeftMenu = function() {
+        $mdSidenav('menue').toggle();
+        };
+
+     }
