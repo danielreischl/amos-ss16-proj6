b937cc3f2e6a2b808d80bbe4ad1a0bcc88309c69
Peter Tan <dead-master@msn.com>
2016-05-09 23:03:16 +0200

    graph example from tobi  incorporated into webApp for testing purpose

--- Client/main.ctrl.js
+++ Client/main.ctrl.js
@@ -7,12 +7,20 @@ angular.module('app')
     vm.title = 'Welcome to the Main Page';
 })
 
-.controller('sideNavController', sideNavController);
-     function sideNavController ($scope, $mdSidenav) {
-        $scope.openLeftMenu = function() {
+.controller('sideNavController', function($scope, $mdSidenav) {
+ $scope.openLeftMenu = function() {
         $mdSidenav('menue').toggle();
         };
+})
 
-     }
 
-
+.controller('visuController', function($scope) {
+    $scope.graphs = [
+        {name : "Acceleration", file : "sections/graphExample/dummy.csv"},
+        {name : "Power", file : "sections/graphExample/dummy2.csv"},
+	];
+    $scope.paintGraph = function(file) {
+	g2 = new Dygraph(
+	    document.getElementById("graphdiv2"), file, {});
+    }
+});
