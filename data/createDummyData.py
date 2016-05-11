5509e4a1f6b4bcbc3c98e13e4ebfd28bca55c708
tobido <tobias.dorsch@fau.de>
2016-05-11 09:42:47 +0200

    Example functions are now splines.

--- data/createDummyData.py
+++ data/createDummyData.py
@@ -1,50 +1,52 @@
 import numpy as np
 import math
 from matplotlib import pyplot as plt
+from scipy import interpolate
 
 # a script to create dummy data -- preliminary version
-# TODO: documentation, restructuring, csv export, bugfixes   
+# TODO: documentation, restructuring, bugfixes   
 
-lineLength = 10000
-driveLength = 5000
-T1 = 50
-T2 = 50
+lineLength = 200
+driveLength = 50
+T1 = 100
+T2 = T1 + 100
+T3 = T2 + 100
+endTime = T3 
+x = np.array([-20, 0,T1,T2,T3, T3+50])
+y = np.array([0, 0,driveLength,3*driveLength,4*driveLength + 5, 4*driveLength + 5])
+spline = interpolate.UnivariateSpline(x, y, k=3, s=0)
 
 # the function describe position/velocity/acceleration of
 # carriers RELATIVE to their entry point
 
 def positionFunction(time):
-    if time < T1:
-        return (1 - np.cos(time * np.pi / (2 * T1))) * driveLength
-    else:
-        return (time - T1) * (np.pi / (2 * T1)) * driveLength + driveLength
+    return spline(time)
 
 
 def velocityFunction(time):
-    if time < T1:
-        return (np.sin(time * np.pi / (2 * T1))) * np.pi / (2 * T1) * driveLength
-    else:
-        return (np.pi / (2 * T1)) * driveLength
-
+    velocity = spline.derivative()
+    return velocity(time)
 
 def accelerationFunction(time):
-    if time < T1:
-        return np.cos(time * np.pi / (2 * T1)) / (4 * T1 * T1) * np.pi * np.pi * driveLength
-    else:
-        return 0
+    acceleration = spline.derivative(n=2)
+    return acceleration(time)
+   
 
-
-def energyFunction(time):
+def energyFunction(time, driveId):
     mass = 1
-    return mass * velocityFunction(time) * accelerationFunction(time)
+    return mass * velocityFunction(time) * accelerationFunction(time) + contaminationFunction(driveId)
+
+def contaminationFunction(driveId):
+    # add contamination here that depends on the drive
+    return 0
 
 
 class Carrier:
     entryTime = -1  # time when carriers enters line
 
-    def __init__(self, entryTime, endTime):
-        self.entryTime = entryTime  # TODO make this a global variable
-        self.endTime = endTime
+    def __init__(self, entryTime,carrierId):
+        self.entryTime = entryTime  
+        self.carrierId = carrierId
 
     def getPosition(self, time):
         # get position of this carrier with respect to the global time frame
@@ -59,17 +61,15 @@ class Carrier:
         if time < self.entryTime:
             return 0
         else:
-            return energyFunction(time - self.entryTime)
+            return energyFunction(time - self.entryTime, self.carrierId)
 
 
 class Drive:
     # variables data store position and energy data
-    endTime = 0  # TODO: make global
     drivePosition = 0  # position where the drive 'begins'
 
-    def __init__(self, drivePosition, endTime, driveNumber):
+    def __init__(self, drivePosition, driveNumber):
         self.drivePosition = drivePosition
-        self.endTime = endTime
         self.data = np.zeros((endTime + 1, 3))
         self.driveNumber = driveNumber
         for time in range(endTime+1):
@@ -100,21 +100,21 @@ class Data:
     numberOfCarriers = 0
     numberOfDrives = 0
     driveLength = 0
-    endTime = 0
     carriers = []
     drives = []
 
-    def __init__(self, entryTimes, numberOfDrives, driveLength, endTime):
+    def __init__(self, entryTimes, numberOfDrives, driveLength):
         self.driveLength = driveLength
         self.numberOfDrives = numberOfDrives
-        self.endTime = endTime
         self.numberOfCarriers = len(entryTimes)
 
+        carrierId = 0        
         for entryTime in entryTimes:
-            self.carriers.append(Carrier(entryTime, endTime))
+            self.carriers.append(Carrier(entryTime, carrierId))
+            carrierId = carrierId + 1
 
         for driveNumber in range(numberOfDrives):
-            self.drives.append(Drive(driveNumber * driveLength, endTime, driveNumber))
+            self.drives.append(Drive(driveNumber * driveLength, driveNumber))
 
     def getDrive(self, position):
         # returns the drive to which the position belongs
@@ -125,7 +125,7 @@ class Data:
             return self.drives[driveNumber]
 
     def createDummy(self):
-        for time in range(self.endTime + 1):
+        for time in range(endTime + 1):
             for carrier in self.carriers:
                 # for each carrier get the drive that it is on
                 position = carrier.getPosition(time)
@@ -133,12 +133,12 @@ class Data:
                 if drive is not None:
                     drive.addPositionPoint(time, position, carrier.getEnergy(time))
 
-    def exportToCsv(self):
-        for drive in drives:
+    def export2Csv(self):
+        for drive in self.drives:
             drive.export2Csv()
 
 
-d = Data([1, 60, ], 2, 5000, 100)
+d = Data([5, 150, 300, 500, 700], 5, 200)
 
 d.createDummy()
 d.export2Csv()
