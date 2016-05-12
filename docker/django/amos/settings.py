29fe7a5a528f01448f3f2da2f8315815d3c6a686
AMOSus <daniel.reischl@outlook.com>
2016-05-12 16:30:31 +0200

    Added DB Information

--- docker/django/amos/settings.py
+++ docker/django/amos/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = 'amos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'postgresql_psycopg2',
+        'NAME': 'docker',
+        'USER': 'docker',
+        'PASSWORD': 'docker',
+        'HOST': 'localhost'
+}
 }
 
 
