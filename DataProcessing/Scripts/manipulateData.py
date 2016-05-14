7e07c467aa4e5ebe6d62cf2b965ba327a8ecd6ac
AMOSus <daniel.reischl@outlook.com>
2016-05-14 17:45:22 +0200

    manipulateData.py creates for each File a pd.Dataframe for regular and manipulated datapoints
    1. The script opens each file in the folder
    2. Creates a DataFrame out of it
    3. Calculates and Adds the columns: Session, Carrier, Iteration, Speed,
    Acceleration for each datapoint
    4. Creates a DataFrame for each Iteration with comulated Values:
    avgEnergyConsumption, comEnergyConsumption, avgSpeed, avgAcceleration

--- DataProcessing/Scripts/manipulateData.py
+++ DataProcessing/Scripts/manipulateData.py
@@ -18,7 +18,7 @@ sleep(0.5)
 
 #Function that checks if new csv files are in the folder
 def check_folder():
-    for files in glob.glob("CleanedDataFiles/*.csv"):
+    for files in glob.glob("CarrierData/*.csv"):
         DATA_FILE_NAMES.append(files)
 
     # Returns False or True depending on where file names are stored in the list or not
@@ -31,7 +31,60 @@ def check_folder():
 def process_file(fileName):
     #Loading Path of the file
     filePath = os.path.abspath(fileName)
-    Data = pd.read_csv(filePath, setConstants.CSV_SEPARATOR, index_col=0)
+    #Loading data into a dataFrame
+    data = pd.read_csv(filePath, setConstants.CSV_SEPARATOR)
+    #Change name of Columns
+
+    #TODO: positionDrive will be added as soon as the data is in the input
+    #TODO: Rename all Columns to the final names
+    data.columns = ['ms','positionABS','energy']
+    # Reads out session of file name and add Column to DataFrame
+    data['session'] = fileName.split('_')[1]
+    # Reads out carrier of file name and add Column to DataFrame
+    data['carrier'] = fileName.split('_')[3]
+    # Reads out iteration of file name and add Column to DataFrame
+    data['iteration'] = fileName.split('_')[5].replace('.csv','')
+    #calculates the speed between two datapoints (Way/Time)
+    data['speed'] = data['positionABS'].diff().divide(data['ms'].diff())
+    #Calculates acceleration (SpeedEnd * SpeedEnd - SpeedBeginn * SpeedBeginn))/distance * 2
+    data['acceleration']= data['speed'].multiply(data['speed']).diff().divide(data['positionABS'].diff().multiply(2))
+
+    #calls function to load the data into the database
+    load_to_database(data)
+
+    #calls function to load and comulate the data into the database
+    load_to_database_comulated(data)
+
+    #TODO: moves the file to the Archive
+
+
+#Loads data into the Database. Input = DataFrame
+def load_to_database(data):
+
+    print "Loading DataFrame into Database"
+
+    #TODO: Load data to database
+    #data.to_sql()
+
+# Comaulates and loads the data into the database. Input =DataFrame
+def load_to_database_comulated(data):
+    # Calculates Average Energy Consumption
+    averageEnergyConsumption = data['energy'].mean()
+    # Calculates Comulated Energy Consumption
+    comulatedEnergyConsumption = data['energy'].sum()
+    # Calculates Average Speed
+    averageSpeed = data['speed'].mean()
+    # Calculates Average Acceleration
+    averageAcceleration = data['acceleration'].mean()
+
+    #Inizialize DataFrame comulatedData
+    comulatedData = pd.DataFrame(columns=['averageSpeed', 'averageEnergy', 'comEnergy', 'averageAcceleration'], index=['1'])
+    #Adding Values to DataFrame
+    comulatedData.loc['1'] = pd.Series ({'averageSpeed':1, 'averageEnergy':averageEnergyConsumption, 'comEnergy':comulatedEnergyConsumption, 'averageAcceleration':averageAcceleration})
+
+    # TODO: Load data to database
+    #comulatedData.to_sql()
+    print "Loading comulated Dataframe to Database"
 
 while os.path.isfile("Running.txt"):
 
@@ -39,12 +92,13 @@ while os.path.isfile("Running.txt"):
     DATA_FILE_NAMES = []
     if check_folder():
         for filename in DATA_FILE_NAMES:
-            #calls the function process_file with each filename stored in DATA_FILE_NAMES
-            process_file(filename)
+            #calls the function process_file with each filename stored in DATA_FILE_NAMES and casts it into a string
+            process_file(str(filename))
 
     # Sleep Time before the script checks again if the file exists.
     sleep(setConstants.WAIT_TIME_IN_SECONDS_MPY)
 
+    # Terminates script for test purposes
     sys.exit()
 
 else:
@@ -59,6 +113,3 @@ else:
             process_file(filename)
 
     sys.exit()
-
-
-
