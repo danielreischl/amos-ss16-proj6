44f3f8fec8ac99db8a0b897aff268334e854a7fc
Inkibus <rathmannrene@hotmail.de>
2016-05-14 20:58:17 +0200

    Deleted two classes that were still included in the models.py from previous testing. Now only the tables of the final DB are left.

--- docker/django/helloWorld/models.py
+++ docker/django/helloWorld/models.py
@@ -1,13 +1,5 @@
 from django.db import models
 
-class Carrier(models.Model):
-    mass = models.FloatField()
-
-class Time(models.Model):
-    carrier = models.ForeignKey(Carrier, on_delete=models.CASCADE)
-    timestamp = models.IntegerField()
-    position = models.FloatField()
-
 # Database model
 # Author: Inkibus (Rene R.)
 
