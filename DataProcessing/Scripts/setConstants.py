a06f4afa7532ba341da1e4a4e94df612d4afb94e
Daniel Reischl <AMOSus@users.noreply.github.com>
2016-05-17 12:53:35 +0200

    updated tablenames
    In the actual build the tablenames are different. helloWorld_tbl_IterationData instead of tbl_IterationData

--- DataProcessing/Scripts/setConstants.py
+++ DataProcessing/Scripts/setConstants.py
@@ -26,7 +26,7 @@ NAME_TABLE_SESSION = 'tbl_Session'
 NAME_TABLE_CARRIER = 'tbl_Carrier'
 NAME_TABLE_ITERATION = 'tbl_Iteration'
 # Name of Table for processed data
-NAME_TABLE_PROCESSED_DATA = 'tbl_TimeStampData'
+NAME_TABLE_PROCESSED_DATA = 'helloWorld_tbl_TimeStampData'
 # Name of Table for commulated data
-NAME_TABLE_COM_DATA = 'tbl_IterationData'
+NAME_TABLE_COM_DATA = 'helloWorld_tbl_IterationData'
 
