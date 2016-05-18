e952556ec96d5a0442f72bce411468433749255f
tobido <tobias.dorsch@fau.de>
2016-05-18 09:00:52 +0200

    Turned debug mode off for production

--- docker/django/amos/settings.py
+++ docker/django/amos/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&pepl5rhqmto-#1vidma9a9$)@fpbp#ex&bz-=mr3%^n6g6w$+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
