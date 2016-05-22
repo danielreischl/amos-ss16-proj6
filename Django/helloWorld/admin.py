6aa8fdb2a7ef73f4e0088d2c40e6e167fdb439fe
Inkibus <rathmannrene@hotmail.de>
2016-05-22 21:18:53 +0200

    Regirstered the right tables.

--- Django/helloWorld/admin.py
+++ Django/helloWorld/admin.py
@@ -1,13 +1,7 @@
 from django.contrib import admin
-from .models import session
-from .models import carrier
-from .models import iteration
 from .models import timestampdata
 from .models import iterationdata
 
 # Register your models here.
-admin.site.register(session)
-admin.site.register(carrier)
-admin.site.register(iteration)
 admin.site.register(timestampdata)
 admin.site.register(iterationdata)
