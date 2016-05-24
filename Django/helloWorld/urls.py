151b3f64b93efaa880389cfb49585a1e8cd039f4
AMOSus <daniel.reischl@outlook.com>
2016-05-24 12:03:05 +0200

    Changed the URLS to call csv-files and values
    Changed position.csv to csv.request and values to values.request to
    enable a consitent url structure

--- Django/helloWorld/urls.py
+++ Django/helloWorld/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^position.csv', views.db2csv, name='position'),
+    url(r'^csv.request', views.db2csv, name='position'),
 
     # Added the url values to enable the frontend to request values like LastItteratioOfaCarrier
     # Calls views.db2values (Further Information views.py)
-    url(r'^values', views.db2values, name='values'),
+    url(r'^values.request', views.db2values, name='values'),
 ]
