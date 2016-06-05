18255d1f896c8ecbac239068fc62032666076e6c
AMOSus <daniel.reischl@outlook.com>
2016-06-05 14:45:40 +0200

    TESTING: Test if python unit test are causing Travis to break
    Wrote a small unit that checks if all folders are existing. To see if
    Travis breaks on failure, named the last folder TestNameForTesting
    instead of CarrierDataArchive

---
+++ DataProcessing/dataProcessingTests.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+#   This file is part of Rogue Vision.
+#
+#   Copyright (C) 2016 Daniel Reischl, Rene Rathmann, Peter Tan,
+#       Tobias Dorsch, Shefali Shukla, Vignesh Govindarajulu,
+#       Aleksander Penew, Abhinav Puri
+#
+#   Rogue Vision is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU Affero General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   Rogue Vision is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU Affero General Public License for more details.
+#
+#   You should have received a copy of the GNU Affero General Public License
+#   along with Rogue Vision.  If not, see <http://www.gnu.org/licenses/>.
+
+# This files includes all tests for dataProcessing
+import unittest
+import os
+
+# This class tests if all necessary folders exist
+class TestFolderExistence (unittest.TestCase):
+
+    # Folder InitialData
+    def testFolderInitialData(self):
+        res = True
+        self.assertEqual(res, os.path.isdir("InitialData"))
+
+    # Folder InitialDataArchive
+    def testFolderInitialDataArchive(self):
+        res = True
+        self.assertEqual(res, os.path.isdir("InitialDataArchive"))
+
+    # Folder CarrierData
+    def testFolderCarrierData(self):
+        res = True
+        self.assertEqual(res, os.path.isdir("CarrierData"))
+
+    # Folder CarrierDataArchive
+    def testFolderCarrierDataArchuve(self):
+        res = True
+        self.assertEqual(res, os.path.isdir("TestNameForTesting"))
+
+    if __name__ == '__main__':
+        unittest.main()
