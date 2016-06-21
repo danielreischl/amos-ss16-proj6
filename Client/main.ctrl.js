2e608d4a3e04b224103ac5e4f0674fa6d122d1a1
Peter Tan <dead-master@msn.com>
2016-06-21 14:06:08 +0200

    IMPLEMENTED: Flexibility graph can now be seen by the user. The user will be able to select multiple carriers and compare the speed over absolute time. Additionally, he can select from which iteration and session, the data should be loaded from.

--- Client/main.ctrl.js
+++ Client/main.ctrl.js
@@ -655,12 +655,12 @@ which kind of data he wants to see. The default value is average energy consumpt
         // the array variable where the converted content from the csv file will be.
         var carrierPercentageData;
         // get the csv files with the percentages from the middleware, extract the exact array and save it into a variable.
-        Papa.parse('django/dataInterface/percentages_creeping.csv?session=1', { download: true,
-                                                                       dynamicTyping: true,
-                                                                       complete: function(results) {
-                                                                           carrierPercentageData =results.data[1];
-                                                                       }
-                                                                      }
+        Papa.parse('django/dataInterface/percentages_creeping.csv?session=1', {download: true,
+                                                                               dynamicTyping: true,
+                                                                               complete: function(results) {
+                                                                                   carrierPercentageData =results.data[1];
+                                                                               }
+                                                                               }
         )
 
         // this array saves the percentage of each bar column/carrier
@@ -787,4 +787,112 @@ which kind of data he wants to see. The default value is average energy consumpt
         return arraySimulationFileNames;
 
     }
+})
+
+/* controller for the Flexibility Chart. This chart will display the speed data of each carrier over absolute time. The user can select
+the session, iterations and carriers he wans to see. */
+    .controller('FlexibilityChartController', function($scope, carrierService, percentageService, sessionService) {
+
+    // get the array with the carriers the user wants to see in the graph.
+    var carrierCompareList = carrierService.getCarrier();
+
+    // Sets the initial time for the time stamp
+    $scope.ts = new Date();
+
+    $scope.selectedIteration = "1";
+
+    $scope.sessions = [];
+    for (var i = 1; i <= sessionService.getNumberOfSessions(); i++) {
+	    $scope.sessions.push(i);
+    }
+
+    // the session requested from the database.
+    $scope.currentSession = sessionService.getCurrentSession();
+
+    //a string, which tells the database how many carrier the user is requesting.
+    var carriersRequested = "";
+
+    // Get the maxAmount of Carriers from the database and save it in a variable called amountOfCarriers
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", 'django/dataInterface/values.request?session='+$scope.currentSession+'&carrier=1&iteration=1&value=amountOfCarriers', false );
+    xmlHttp.send(null);
+    var amountOfCarriers = xmlHttp.responseText;
+
+    // Get the last iteration database and save it
+    var xmlHttp2 = new XMLHttpRequest();
+    xmlHttp2.open( "GET", 'django/dataInterface/values.request?session='+$scope.currentSession+'&carrier=1&iteration=1&value=lastIteration', false );
+    xmlHttp2.send(null);
+    var lastIteration = xmlHttp2.responseText;
+
+    //create an array depending on the amount of carriers. The items of the array will be used to initialize the checkboxes.
+    $scope.carriers = [];
+    for (var idCounter = 1; idCounter <= amountOfCarriers;idCounter++) {
+	var currentSelected = carrierService.hasCarrier(idCounter);
+        $scope.carriers.push({id:idCounter, selected:currentSelected});
+    }
+
+    // create the dropdown menu for iterations. the array gets filled with the iteration numbers available in the database.
+    $scope.iterationDimensions = [];
+    for (var i = 1; i <= lastIteration;idCounter++) {
+	    $scope.iterationDimensions.push(i);
+    }
+
+    // make percentage service available in html-view
+    // not very nice, try to refactor if possible
+    $scope.percentageService = percentageService;
+
+    /* this functions creates the dygraph  from a data source and applies options to them*/
+
+    $scope.createFlexibilityChart = function() {
+
+	$scope.carriersRequested = function() {
+	    // filter for the selected carriers
+	    var selected = $scope.carriers.filter(function(carrier){return carrier.selected;});
+
+	    //join them with commas
+
+	    return selected.map(function(carrier){return carrier.id.toString();}).join();
+	}
+
+	sessionService.setCurrentSession($scope.currentSession);
+
+        // create the graph with the parameters set. The request path for the database depends on 3 parameters: carrierRequested, selectedIteration and selectedSession
+        // the url which should be requested wil be defined in requestedUrl
+        // to allow to export the csv file the variable is defined as a $scope variable
+        $scope.requestedUrl = 'django/dataInterface/continuousDataAbsoluteTime.csv?carriers='+$scope.carriersRequested()+'&iterations='+$scope.selectedIteration+'&dimension=speed&session='+$scope.currentSession+'';
+
+        graph = new Dygraph(
+	       document.getElementById("FlexibilityChart"),$scope.requestedUrl ,
+	                                                                                     {title: 'Flexibility Graph',
+	                                                                                      ylabel: 'Speed',
+	                                                                                      xlabel: 'Absolute Time in ms',
+	                                                                                      labelsSeparateLines: true,
+	                                                                                      highlightSeriesOpts: {strokeWidth: 4, strokeBorderWidth: 1, highlightCircleSize: 5},
+	                                                                                      legend: "always",
+	                                                                                      /*labelDiv looks for an element with the given id and puts the legend into this element.
+	                                                                                       Therefore the legend will not bis displayed inside the graph */
+	                                                                                      labelsDiv: document.getElementById("FlexibilityChartLegend"),
+	                                                                                      /* formatting the x axis label in the legend. Now it will display not only the value but also a text */
+	                                                                                      axes: {
+	                                                                                        x: {
+                                                                                                valueFormatter: function(x) {
+                                                                                                    return 'Iteration ' + x;
+                                                                                                },
+                                                                                            },
+                                                                                          }
+	                                                                                      });
+
+	$scope.getListStyle = function(index) {
+	    if (index % 5 == 1) {
+		return {'clear': 'left'};
+	    }
+	    else {
+		return {};
+	    }
+	}
+
+        // Updates the  time for the time stamp
+        $scope.ts = new Date();
+    }
+
 })
