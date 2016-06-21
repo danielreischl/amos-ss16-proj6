fb7a21838f4e5a71d45c74739f4a8d3d6db172fe
Peter Tan <dead-master@msn.com>
2016-06-21 21:23:50 +0200

    TESTING: testing a wait time of 10 seconds, as the previous increase did not fix the previous issue

--- Client/main.ctrl.js
+++ Client/main.ctrl.js
@@ -403,9 +403,8 @@ which kind of data he wants to see. The default value is average energy consumpt
  and upon receiving an event, it should trigger the update circle button*/
 .controller('circleGraphController', function($scope, $compile, $mdDialog, $mdMedia, $timeout, $mdSidenav, carrierService, percentageService) {
     // title will change, depending on which circleView is showing.
-    var circleTitle = "Creeping Contamination";
     var changed = 0;
-    $scope.circleView_title = circleTitle;
+    $scope.circleView_title = "Creeping Contamination";
 
     // data variables to be changed
     var percentageDataType = "percentages_creeping.csv";
@@ -415,13 +414,13 @@ which kind of data he wants to see. The default value is average energy consumpt
     $scope.changeView = function() {
        if(changed == 0) {
            percentageDataType = "percentages_cont.csv";
-           circleTitle = "Continuous Contamination";
+           $scope.circleView_title = "Continuous Contamination";
            changed = 1;
            clearCanvas();
            $scope.circleGraph();
        } else {
            percentageDataType = "percentages_creeping.csv";
-           circleTitle = "Creeping Contamination";
+           $scope.circleView_title = "Creeping Contamination";
            changed = 0;
            clearCanvas();
            $scope.circleGraph();
@@ -498,7 +497,7 @@ which kind of data he wants to see. The default value is average energy consumpt
     var idCounter = 1;
 
     //delay the creation of the circles by 1 second, so that the percentage data can be loaded into the function.
-    $timeout(createCarrierHTML, 2000);
+    $timeout(createCarrierHTML, 10000);
 
     // function to create HTML circle fragments dynamically
     function createCarrierHTML() {
